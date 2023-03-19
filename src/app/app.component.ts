33c13beb1ea506e5fa711562977847024901fe76
Nitin Srivastava <nitin.dev3d@gmail.com>
2023-03-20 01:39:11 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pass-generator';
+  password = ""
+  includeLetters = false
+  includeNumbers = false
+  includeSymbols = false
+  length = 0
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols
+  }
+
+  onChangeLength(value: string) {
+    this.length = Number(value) 
+  }
+  onButtonClick() {
+    this.password = ""
+    const numbers = '1234567890'
+    const letters = 'abcdefghijklmnopqrstuvwxyz'
+    const symbols = '!@#$%^&*()'
+    let validChars = ""
+    if(this.includeLetters) validChars += letters
+
+    if(this.includeNumbers) validChars += numbers
+
+    if(this.includeSymbols) validChars += symbols
+
+    let validCharsLength = validChars.length
+    
+    for (let i = 0; i < this.length; i++)
+      this.password += validChars[Math.floor(Math.random() * validCharsLength)]
+
+    // console.log(this.password);
+    
+  }
+  
 }
